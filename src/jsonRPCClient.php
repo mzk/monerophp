c0f5054dfa28a60b81f7597d9756a645bce33d42
Olda Salek <mzk@mozektevidi.net>
2018-10-05 16:04:32 +0200

    higher timeout for requests

--- src/jsonRPCClient.php
+++ src/jsonRPCClient.php
@@ -14,8 +14,8 @@ class jsonRPCClient
     private $username;
     private $password;
     protected $curl_options = array(
-        CURLOPT_CONNECTTIMEOUT => 8,
-        CURLOPT_TIMEOUT => 8
+        CURLOPT_CONNECTTIMEOUT => 60,
+        CURLOPT_TIMEOUT => 60
     );
 
 
